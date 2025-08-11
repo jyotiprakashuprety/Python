8010b1e2644a1e401948751d81f86bd46468bf95
Jyoti Prakash Uprety <55077730+jyotiprakashuprety@users.noreply.github.com>
2025-08-10 23:18:22 -0400

    basic

--- 01_input_output.py
+++ 01_input_output.py
@@ -1,32 +1,39 @@
 # Ask the user for their name and age, then print: "Hello John! You are 25 years old."
 
-name = input("Enter your Name:")
+# name = input("Enter your Name:")
 
-while True:
-    age = input("Enter your age:")
-    if age.isdigit():
-        age = int(age)
-        if 0<= age <= 120:
-            break
-        else:
-            print("Age must be between 0 to 120")
-    else:
-        print("Please enter the valid age!")
-print(f"Hello {name}! You are {age} years old")
+# while True:
+#     age = input("Enter your age:")
+#     if age.isdigit():
+#         age = int(age)
+#         if 0<= age <= 120:
+#             break
+#         else:
+#             print("Age must be between 0 to 120")
+#     else:
+#         print("Please enter the valid age!")
+# print(f"Hello {name}! You are {age} years old")
 
 
-# Get input from the user
-total_minutes = int(input("Enter the number of minutes: "))
+# # Get input from the user
+# total_minutes = int(input("Enter the number of minutes: "))
 
-# Calculate hours and minutes
-hours = total_minutes // 60
-minutes = total_minutes % 60
+# # Calculate hours and minutes
+# hours = total_minutes // 60
+# minutes = total_minutes % 60
 
-# Display result
-print(f"{total_minutes} minutes = {hours} hour(s) and {minutes} minute(s)")
+# # Display result
+# print(f"{total_minutes} minutes = {hours} hour(s) and {minutes} minute(s)")
 
 
 # Take two numbers as input, and print their sum, difference, product, and quotient.
+num1 = int(input("Enter number 1"))
+num2 = int(input("Enter number 2"))
+
+
+sum = num1+num2
+
+
 
 # Ask for a string and print it in uppercase, lowercase, and title case.
 
